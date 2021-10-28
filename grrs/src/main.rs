4ed8a073f3ed6503d1735a24da4bc98e8f41d90b
vasanthonline <vasanthgonline@gmail.com>
2021-10-28 18:10:53 +0530

    Add logs with descriptive messages

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,6 +1,8 @@
 use structopt::StructOpt;
 use std::io::{prelude::*, BufReader};
 use std::fs::File;
+use log::{info};
+
 
 #[derive(StructOpt)]
 struct Cli {
@@ -12,6 +14,7 @@ struct Cli {
 
 fn main() {
     let args = Cli::from_args();
+    info!("Searching for word '{:?}' in '{:?}'", &args.pattern, &args.path );
     let file = File::open(&args.path).expect("Could not read file.");
     let reader = BufReader::new(file);
     for line in reader.lines() {
