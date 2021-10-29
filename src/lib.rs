3f3973c0603053d9c41335bee2a6841ee9065d09
vasanthonline <vasanthgonline@gmail.com>
2021-10-29 17:19:22 +0530

    Move tests within source code

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-
 pub fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
     for line in content.lines() {
-        if line.contains(pattern) {
+        if line.contains(&pattern.to_lowercase()) {
             let result = writeln!(writer, "{}", line);
             match result {
                 Ok(_) => (),
@@ -9,4 +8,26 @@ pub fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Writ
             }
         }
     }
+}
+
+
+#[test]
+fn should_find_matches() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
+}
+
+#[test]
+fn should_find_zero_matches() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "loram", &mut result);
+    assert_eq!(result, b"");
+}
+
+#[test]
+fn should_find_case_insensitive_matches() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "LOREM", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
 }
--- src/main.test.rs
+++
@@ -1,6 +0,0 @@
-#[test]
-fn should_find_matches() {
-    let mut result = Vec::new();
-    grep::find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
-    assert_eq!(result, b"lorem ipsum\n");
-}
